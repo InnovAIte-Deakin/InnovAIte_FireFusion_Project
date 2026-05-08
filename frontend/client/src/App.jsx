ef964dc87dccb8ace96061317fd180d440460620
RitikaSharma2815 <RITIKASHARMA2815@gmail.com>
2026-05-08 17:44:42 +1000

    Convert login and signup pages to React components

--- frontend/client/src/App.jsx
+++ frontend/client/src/App.jsx
@@ -1,11 +1,21 @@
 import Dashboard from "./pages/Dashboard.jsx";
 import FireRiskMap from "./pages/FireRiskMap.tsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 
 export default function App() {
   const path = window.location.pathname;
 
   if (path === "/fire-map") {
     return <FireRiskMap />;
+  }
+
+  if (path === "/login") {
+    return <Login />;
+  }
+
+  if (path === "/signup") {
+    return <Signup />;
   }
 
   return <Dashboard />;
